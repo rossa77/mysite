e623d36c14d22c343fdcbeb6302fd90fca9ff2f7
sara <sarrarostami77@gmail.com>
2024-08-24 00:51:50 +0330

    commit

--- employer/admin.py
+++ employer/admin.py
@@ -29,3 +29,43 @@ class EmployerAdmin(admin.ModelAdmin):
         return True
 
 admin.site.register(Employer,EmployerAdmin)
+
+
+class JobPostingAdmin(admin.ModelAdmin):
+    list_display = ('title', 'company', 'category', 'salary', 'posting_date', 'expiry_date')
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(company__owner=request.user)
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.company = Employer.objects.get(owner=request.user)
+        obj.save()
+
+    def has_add_permission(self, request):
+        if request.user.is_superuser:
+            return True
+        try:
+            if request.user.is_authenticated:
+               Employer.objects.get(owner=request.user)
+               return True
+        except Employer.DoesNotExist:
+            return False
+
+
+    def has_change_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        if obj:
+            return obj.company.owner == request.user
+        return super().has_change_permission(request, obj)
+
+    def has_view_permission(self, request, obj=None):
+        return True
+
+    def has_module_permission(self, request, obj=None):
+        return True
+admin.site.register(JobPosting, JobPostingAdmin)
